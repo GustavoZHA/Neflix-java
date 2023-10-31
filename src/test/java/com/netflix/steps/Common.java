3db8d9801e6a352c2294465c984cd9d9a10c04a5
Gustavo Huanca <gustavozha@gmail.com>
2023-10-31 19:19:18 -0400

    Add logs info

--- src/test/java/com/netflix/steps/Common.java
+++ src/test/java/com/netflix/steps/Common.java
@@ -6,11 +6,14 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import org.apache.logging.log4j.LogManager;
 import org.testng.Assert;
+import org.apache.logging.log4j.Logger;
 
 public class Common {
     private Navigate navigate;
     private WebDriverAction action;
+    private Logger log = LogManager.getLogger(getClass());
 
     public Common() {
         navigate = new Navigate();
@@ -29,11 +32,15 @@ public class Common {
 
     @Then("^I verify tittle should be \"(.*?)\"$")
     public void verifiesTittle(String tittle) {
-        Assert.assertEquals(action.getTittle(), tittle, "The tittle should be:" + tittle);
+        String tittleActual = action.getTittle();
+        log.info(tittleActual);
+        Assert.assertEquals(tittleActual, tittle, "The tittle should be:" + tittle);
     }
 
     @Then("^I verify url should be \"(.*?)\"$")
     public void verifiesUrl(String url) {
-        Assert.assertEquals(action.getUrl(), url, "The url should be:" + url);
+        String urlActual = action.getUrl();
+        log.info(urlActual);
+        Assert.assertEquals(urlActual, url, "The url should be:" + url);
     }
 }
